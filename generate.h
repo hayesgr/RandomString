92e4cdf162410d2e390a2d3ff9837bcb9e179b50
hayesgr <42255528+hayesgr@users.noreply.github.com>
2024-02-07 22:59:27 -0600

    Create generate.h
    initial

---
+++ generate.h
@@ -0,0 +1,9 @@
+#ifndef GENERATE_H_INCLUDED
+#define GENERATE_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int generate_str(char **str,unsigned int type, unsigned int length);
+
+
+#endif // GENERATE_H_INCLUDED
