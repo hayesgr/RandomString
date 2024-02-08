7df3435eed91fa78ebe0113f214a68804b12d931
hayesgr <42255528+hayesgr@users.noreply.github.com>
2024-02-08 00:11:23 -0600

    Update generate.c
    Added reduced code in comments

--- generate.c
+++ generate.c
@@ -37,3 +37,28 @@ int generate_str(char **str,unsigned int type, unsigned int length){
     *str = temp_str;
     return 1;
 }
+//Alternately
+/*
+int generate_str(char **str,unsigned int type, unsigned int length){
+    srand(time(NULL));
+    unsigned int temp=type;
+    char lower[] = "abcdefghijklmnopqrstuvwxyz";
+    char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char numbers[] = "0123456789";
+    char symbols[] = {33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,58,59,60,61,62,63,64,91,92,93,94,95,96,123,124,125,126};
+    char *arrs[4] = {lower,upper,numbers,symbols};
+    //Allocate temp string
+    char *temp_str = malloc(sizeof(char)*length+1);
+    if(!temp_str){return -1;}
+    temp_str[length]='\0';
+    //File usable array with usable characters selected
+    temp = type;
+    for(unsigned int i=0;i<length;i++){
+        if(temp==0){temp=type;}
+        temp_str[i]=arrs[(temp%10)-1][rand()%(sizeof(arrs[temp%10])-1)];
+        temp/=10;
+    }
+    *str = temp_str;
+    return 1;
+}
+*/
