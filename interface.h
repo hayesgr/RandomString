f14a2a99e432fb380d63235400bdc71383fd58a1
hayesgr <42255528+hayesgr@users.noreply.github.com>
2024-02-07 22:58:53 -0600

    Create interface.h
    initial

---
+++ interface.h
@@ -0,0 +1,9 @@
+#ifndef INTERFACE_H_INCLUDED
+#define INTERFACE_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int string_size();
+unsigned int string_type();
+
+#endif // INTERFACE_H_INCLUDED
